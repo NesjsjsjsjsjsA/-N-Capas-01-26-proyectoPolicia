35c4ed29eaabc5530a34ea26925024c84d127e9b
Néstor Ayala <127457394+NesjsjsjsjsjsA@users.noreply.github.com>
2026-05-19 02:52:15 -0600

    Ad

---
+++ Central_Policia/src/main/java/com/example/central_policia/CentralPoliciaApplication.java
@@ -0,0 +1,13 @@
+package com.example.central_policia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralPoliciaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CentralPoliciaApplication.class, args);
+    }
+
+}
---
+++ Central_Policia/src/test/java/com/example/central_policia/CentralPoliciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.central_policia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralPoliciaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
